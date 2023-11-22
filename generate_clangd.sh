f003ac92fc287e54cd7dd7a0874776e439e93f59
Louis Montaut <louismontaut@gmail.com>
2023-11-22 01:52:34 +0100

    Script to generate .clangd config file
    In a shell, `export PASTEL_DIR=$(pwd)` when inside pastel dir

---
+++ generate_clangd.sh
@@ -0,0 +1,41 @@
+echo "
+CompileFlags:
+    Add: [-xc,
+        -Wall,
+        -Wextra,
+        -std=c99,
+        -I$PASTEL_DIR]
+---
+If:
+    PathMatch: pastel.h
+CompileFlags:
+    Add: [-DPASTEL_IMPLEMENTATION]
+---
+If:
+    PathMatch: third-party/stb_image.h
+CompileFlags:
+    Add: [-DSTB_IMAGE_IMPLEMENTATION]
+---
+If:
+    PathMatch: third-party/stb_image_write.h
+CompileFlags:
+    Add: [-DSTB_IMAGE_WRITE_IMPLEMENTATION]
+
+# Other .clangd example:
+# Fragment common to C and C++ source files
+# CompileFlags:
+#     Add:
+#         - "--include-directory=some/directory/to/search/includes/in"
+#         - "-D"
+#         - "SOME_MACRO_TO_DEFINE"
+#         - "-include"
+#         - "some/file/to/force/inclusion/of.h"
+#
+# ---
+# Fragment specific to C++ source files
+# If:
+#     PathExclude: [.*\.c, .*\.h]
+# CompileFlags:
+#     Add:
+#         - "-std=c++17"
+" > .clangd
