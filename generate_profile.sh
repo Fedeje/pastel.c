ae06fec1bea603dd9960a48b2f489d83b1a2cd53
Louis Montaut <louismontaut@gmail.com>
2023-11-27 11:56:22 +0100

    Fix `generate_profile.sh` for macos

--- generate_profile.sh
+++ generate_profile.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 echo "\
+#!/bin/sh
 # Please fill these two lines:
 SDL_LIB_PATH=""
 SDL_INCLUDE_PATH=""
 
 export SDL_LIB=\$SDL_LIB_PATH
 export SDL_INCLUDE=\$SDL_INCLUDE_PATH
-export PASTEL_DIR=(\$pwd)
+export PASTEL_DIR=(\$PWD)
 
 # if [ -z \"\$SDL_LIB\" ]; then export SDL_LIB=\$SDL_LIB_PATH; fi
 # if [ -z \"\$SDL_INCLUDE\" ]; then export SDL_INCLUDE=\$SDL_INCLUDE_PATH; fi
